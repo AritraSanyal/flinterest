c3dad0330ac16df470dfb3ac19155eb3f0cf39dc
Aritra Sanyal <aritras2003@gmail.com>
2026-05-19 00:11:33 +0530

    feat: refactor main entry point and add App widget

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flinterest/core/theme/app_theme.dart';
+import 'package:flinterest/feature/auth/presentation/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flinterest',
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      home: const WelcomeScreen(),
+    );
+  }
+}
