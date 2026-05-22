c58a8eec77257bf852fd2e19f5c5338adf18fb6f
Aritra Sanyal <aritras2003@gmail.com>
2026-05-22 23:03:19 +0530

    refactor(welcome): added the image collage into this screen

--- lib/feature/auth/presentation/screens/welcome_screen.dart
+++ lib/feature/auth/presentation/screens/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flinterest/core/theme/app_spacing.dart' as sp;
+import 'package:flinterest/feature/auth/presentation/widgets/welcome_collage.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -18,135 +19,123 @@ class WelcomeScreen extends StatelessWidget {
           alignment: .bottomCenter,
           minHeight: availableHeight,
           maxHeight: availableHeight,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            crossAxisAlignment: .center,
+            children: [
+              // ===============================
+              // COLLAGE and logo
+              // ===============================
 
-            child: Column(
-              crossAxisAlignment: .center,
-              children: [
-                // ===============================
-                // COLLAGE and logo
-                // ===============================
+              // placeholder for the collage
+              Stack(
+                clipBehavior: .none,
+                children: [
+                  WelcomeCollage(screenHeight: screenHeight),
 
-                // placeholder for the collage
-                Stack(
-                  clipBehavior: .none,
+                  Positioned(
+                    bottom: 5,
+                    left: 0,
+                    right: 0,
+                    child: SvgPicture.asset(
+                      'assets/flinterest_logo.svg',
+                      height: 38,
+                      width: 38,
+                    ),
+                  ),
+                ],
+              ),
+
+              // ===============================
+              // FORM AND INTERACTIVE BUTTONS
+              // ===============================
+              sp.AppSpacing.gapVsm,
+
+              // HEADER
+              Text(
+                'Create a life\nyou love',
+                textAlign: .center,
+                style: Theme.of(
+                  context,
+                ).textTheme.headlineMedium?.copyWith(fontWeight: .w700),
+              ),
+
+              sp.AppSpacing.gapVlg,
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 46),
+                child: Column(
                   children: [
+                    // ======================
+                    // email
+                    // ======================
+                    TextField(
+                      decoration: InputDecoration(hintText: 'Email address'),
+                    ),
+
+                    sp.AppSpacing.gapVlg,
+
+                    // ======================
+                    // continue button
+                    // ======================
                     SizedBox(
-                      height: screenHeight * 0.40,
                       width: double.infinity,
-                      child: const Placeholder(
-                        color: Colors.grey,
-                        strokeWidth: 2,
+                      height: 35,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          print('continue');
+                        },
+                        child: Text(
+                          'Continue',
+                          style: TextStyle(fontWeight: .w600),
+                        ),
                       ),
                     ),
 
-                    Positioned(
-                      bottom: 5,
-                      left: 0,
-                      right: 0,
-                      child: SvgPicture.asset(
-                        'assets/flinterest_logo.svg',
-                        height: 38,
-                        width: 38,
+                    sp.AppSpacing.gapVlg,
+
+                    // ======================
+                    // Continue with google
+                    // ======================
+                    SizedBox(
+                      width: double.infinity,
+                      // height: ,
+                      child: OutlinedButton(
+                        onPressed: () {
+                          print('Continue with google');
+                        },
+                        child: Stack(
+                          alignment: .centerLeft,
+                          children: [
+                            // google svg
+                            SvgPicture.asset(
+                              'assets/google.svg',
+                              height: 25,
+                              width: 25,
+                            ),
+                            // text
+                            Align(
+                              alignment: .center,
+                              child: Text(
+                                'Continue with Google',
+                                style: Theme.of(context).textTheme.bodyMedium
+                                    ?.copyWith(fontSize: 15, fontWeight: .w600),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ],
                 ),
+              ),
+              sp.AppSpacing.gapVmd,
 
-                // ===============================
-                // FORM AND INTERACTIVE BUTTONS
-                // ===============================
-                sp.AppSpacing.gapVsm,
-
-                // HEADER
-                Text(
-                  'Create a life\nyou love',
-                  textAlign: .center,
-                  style: Theme.of(
-                    context,
-                  ).textTheme.headlineMedium?.copyWith(fontWeight: .w700),
-                ),
-
-                sp.AppSpacing.gapVlg,
-
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 22),
-                  child: Column(
-                    children: [
-                      // ======================
-                      // email
-                      // ======================
-                      TextField(
-                        scrollPadding: EdgeInsets.only(bottom: 300),
-                        decoration: InputDecoration(hintText: 'Email address'),
-                      ),
-
-                      sp.AppSpacing.gapVlg,
-
-                      // ======================
-                      // continue button
-                      // ======================
-                      SizedBox(
-                        width: double.infinity,
-                        height: 35,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            print('continue');
-                          },
-                          child: Text(
-                            'Continue',
-                            style: TextStyle(fontWeight: .w600),
-                          ),
-                        ),
-                      ),
-
-                      sp.AppSpacing.gapVlg,
-
-                      // ======================
-                      // Continue with google
-                      // ======================
-                      SizedBox(
-                        width: double.infinity,
-                        // height: ,
-                        child: OutlinedButton(
-                          onPressed: () {
-                            print('Continue with google');
-                          },
-                          child: Stack(
-                            alignment: .centerLeft,
-                            children: [
-                              // google svg
-                              SvgPicture.asset(
-                                'assets/google.svg',
-                                height: 25,
-                                width: 25,
-                              ),
-                              // text
-                              Align(
-                                alignment: .center,
-                                child: Text(
-                                  'Continue with Google',
-                                  style: Theme.of(context).textTheme.bodyMedium
-                                      ?.copyWith(
-                                        fontSize: 15,
-                                        fontWeight: .w600,
-                                      ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                sp.AppSpacing.gapVmd,
-
-                // ======================
-                // recover facebook login
-                // ======================
-                Text.rich(
+              // ======================
+              // recover facebook login
+              // ======================
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 46),
+                child: Text.rich(
                   textAlign: .center,
                   TextSpan(
                     style: Theme.of(
@@ -165,60 +154,58 @@ class WelcomeScreen extends StatelessWidget {
                     ],
                   ),
                 ),
+              ),
 
-                // ===============================
-                // LEGAL TERMS
-                // ===============================
-                const SizedBox(height: 40),
-                const Spacer(),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 23),
-                  child: Text.rich(
-                    textAlign: .center,
-                    style: Theme.of(context).textTheme.bodySmall,
-                    TextSpan(
-                      children: [
-                        TextSpan(
-                          text: 'By continuing, you agree to Pinterest\'s ',
+              // ===============================
+              // LEGAL TERMS
+              // ===============================
+              const SizedBox(height: 40),
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 46),
+                child: Text.rich(
+                  textAlign: .center,
+                  style: Theme.of(context).textTheme.bodySmall,
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'By continuing, you agree to Pinterest\'s ',
+                      ),
+                      TextSpan(
+                        text: 'Terms of Service',
+                        style: const TextStyle(
+                          decoration: TextDecoration.underline,
                         ),
-                        TextSpan(
-                          text: 'Terms of Service',
-                          style: const TextStyle(
-                            decoration: TextDecoration.underline,
-                          ),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              print('Terms of Service');
-                            },
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            print('Terms of Service');
+                          },
+                      ),
+                      TextSpan(text: ' and acknowledge that you\'ve read our '),
+                      TextSpan(
+                        text: 'Privacy Policy',
+                        style: const TextStyle(
+                          decoration: TextDecoration.underline,
                         ),
-                        TextSpan(
-                          text: ' and acknowledge that you\'ve read our ',
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            print('Privacy Policy');
+                          },
+                      ),
+                      TextSpan(text: '. '),
+                      TextSpan(
+                        text: 'Notice at collection',
+                        style: const TextStyle(
+                          decoration: TextDecoration.underline,
                         ),
-                        TextSpan(
-                          text: 'Privacy Policy',
-                          style: const TextStyle(
-                            decoration: TextDecoration.underline,
-                          ),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              print('Privacy Policy');
-                            },
-                        ),
-                        TextSpan(text: '. '),
-                        TextSpan(
-                          text: 'Notice at collection',
-                          style: const TextStyle(
-                            decoration: TextDecoration.underline,
-                          ),
-                        ),
-                        TextSpan(text: '.'),
-                      ],
-                    ),
+                      ),
+                      TextSpan(text: '.'),
+                    ],
                   ),
                 ),
-                sp.AppSpacing.gapVmd,
-              ],
-            ),
+              ),
+              sp.AppSpacing.gapVmd,
+            ],
           ),
         ),
       ),
