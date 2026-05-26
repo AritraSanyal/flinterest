bd5fc6336fcb99bc302ce008ecf1b805b63a945d
Aritra Sanyal <aritras2003@gmail.com>
2026-05-26 13:03:27 +0530

    style(palette): added color
    -- added new shade fo roboflow ie gray

--- lib/core/theme/app_palette.dart
+++ lib/core/theme/app_palette.dart
@@ -8,8 +8,9 @@ class AppPalette {
   static const Color marshmallow0 = Color(0xffffffff);
   static const Color cosmicore900 = Color(0xff000000);
   static const Color roboflow200 = Color(0xffebebeb);
-  static const Color roboflow500 = Color(0xff767676);
   static const Color roboflow400 = Color(0xffa5a5a5);
+  static const Color roboflow500 = Color(0xff767676);
+  static const Color roboflow800 = Color(0xff424242);
 
   // Status
   static const Color green500 = Color(0xff008753);
